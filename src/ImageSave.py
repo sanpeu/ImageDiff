1b23ffd285cd2b8ccb5661f7c88ada771c07a9f0
sanpeu <udbmmuk41@naver.com>
2024-10-19 11:08:03 +0900

    add mouse moveing to diff

--- src/ImageSave.py
+++ src/ImageSave.py
@@ -12,6 +12,7 @@ from PIL import ImageChops
 # height 625
 
 width, height = 751, 624
+x_pos = 204
 y_pos = 137
 
 src = pyautogui.screenshot(region=(204, y_pos, width, height))
@@ -33,16 +34,24 @@ dest_img = cv2.imread('./image/dest.jpg')
 diff_img = cv2.imread('./image/diff.jpg')
 
 gray = cv2.cvtColor(diff_img, cv2.COLOR_BGR2GRAY) # gray로 바꾸기
+# blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+# edges = cv2.Canny(blurred, 50, 150)
+# cv2.imshow('edges', edges)
 contours, _ = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
 COLOR = (0, 200, 0)
 
 for cnt in contours:
-    if cv2.contourArea(cnt) > 100:
+    if cv2.contourArea(cnt) > 70:
         x, y, width, height = cv2.boundingRect(cnt)
         cv2.rectangle(src_img, (x, y), (x + width, y + height), COLOR, 2)
         cv2.rectangle(dest_img, (x, y), (x + width, y + height), COLOR, 2)
         cv2.rectangle(diff_img, (x, y), (x + width, y + height), COLOR, 2)
+
+        to_x = x + width // 2 + x_pos # 이미지 시작 위치와 컴퓨터에서 보이는 위치 조절
+        to_y = y + height // 2 + y_pos
+        pyautogui.moveTo(to_x, to_y, duration=0.5)
+        # pyautogui.click()
 
 cv2.imshow('src', src_img)
 cv2.imshow('dest', dest_img)
